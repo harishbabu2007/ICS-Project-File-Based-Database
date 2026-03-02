8b79d93109d14245a457eb2b7879a35cea0f7285
Ashhar Ansari <ansifrah@gmail.com>
2026-03-02 23:12:40 +0530

    tokenizer
    made the tokenizer in cpp

---
+++ src/interpreter/tokenizer.cpp
@@ -0,0 +1,33 @@
+#include "interpreter/interpreter.h"
+
+vector<string> tokenize(const string& s) {
+    vector<string> tokens;
+    string x="";
+    int length=s.size();
+    for(int i=0;i<length;i++){
+        char c=s[i];
+        if(isspace(c)){
+            if(!x.empty()){
+                tokens.push_back(x);
+                x="";
+            }
+            continue;
+        }
+        else if((c==',')||(c=='(')||(c==')')||(c==';')){
+                if(!x.empty()){
+                tokens.push_back(x);
+                x="";
+            }
+             tokens.push_back(string(1,c));
+        }
+        else if(c=='"'){
+            
+        }
+        else{
+            x+=c;
+        }
+    }
+       if (!x.empty())
+        tokens.push_back(x);
+    return tokens;
+}
