753eac1d4191d842663d7015586d39b26d278082
Ashhar Ansari <ansifrah@gmail.com>
2026-03-11 16:54:33 +0530

    made some changes in tokenizer and insert

--- src/interpreter/insert_into_table.cpp
+++ src/interpreter/insert_into_table.cpp
@@ -5,307 +5,202 @@
 // void insert_into_table(vector<string> tokens)
 // {
 //     string table_name = tokens[2];
-
-//     schema_t table_schema =
-//     get_schema_from_schema(table_name + "__schema_data.bin");
-
-//     size_t num_cols = table_schema.num_cols;
-
-//     // Find VALUES
-//     int values_index = -1;
-//     for (size_t i = 0; i < tokens.size(); i++)
+//     schema_t new_schema = get_schema_from_schema(table_name + "__schema_data.bin");
+//     size_t num_cols = new_schema.num_cols;
+//     vector<row_data_t> data_to_insert;
+//     auto j = tokens.begin() + 5;
+//     for (size_t i = 0; i < num_cols; i++)
 //     {
-//         if (tokens[i] == "VALUES")
+//         string value = *j;
+//         switch (new_schema.column_data[i].data_type)
 //         {
-//             values_index = i;
+//         case INT:
+//         {
+//             data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<int>(stoi(value))});
+//             j += 2;
 //             break;
 //         }
-//     }
-//     vector<vector<row_data_t>> all_rows;
-//     vector<string> current_row_values;
 
-//     for (size_t i = values_index + 1; i < tokens.size(); i++)
+//         case UNSIGNED_INT:
+//         {
+//             data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<unsigned int>(stoul(value))});
+//             j += 2;
+//             break;
+//         }
+
+//         case STRING:
+//         {
+//             data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<string>(value)});
+//             j += 2;
+//             break;
+//         }
+
+//         case BOOLEAN:
+//         {
+//             bool v = (value == "true");
+//             data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<bool>(v)});
+//             j += 2;
+//             break;
+//         }
+
+//         case FLOATING_POINT:
+//         {
+//             data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<float>(stof(value))});
+//             j += 2;
+//             break;
+//         }
+
+//         case DOUBLE_FLOATING_POINT:
+//         {
+//             data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<double>(stod(value))});
+//             j += 2;
+//             break;
+//         }
+//         case UNSIGNED_CHAR:
+//         {
+//             unsigned char v = static_cast<unsigned char>(stoi(value));
+//             data_to_insert.push_back(
+//                 (row_data_t){static_cast<unsigned char>(i), make_shared<unsigned char>(v)});
+//             j += 2;
+//             break;
+//         }
+
+//         case NULL_TYPE:
+//         {
+//             data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<string>("NULL")});
+//             j += 2;
+//             break;
+//         }
+
+//         default:
+//         {
+//             logger("Unknown data type\n", LOG_ERROR);
+//             return;
+//         }
+//         }
+//     }
+//     int result = append_record_to_table(new_schema, data_to_insert);
+
+//     if (result == 0)
 //     {
-//         if (tokens[i] == "(")
-//         {
-//             current_row_values.clear();
-//         }
-//         else if (tokens[i] == ")")
-//         {
-//             if (current_row_values.size() != num_cols)
-//             {
-//                 logger("Column count mismatch\n", LOG_ERROR);
-//                 return;
-//             }
-
-//             vector<row_data_t> row_data;
-
-//             for (size_t col = 0; col < num_cols; col++)
-//             {
-//                 row_data_t cell;
-//                 cell.col_id = col;
-
-//                 col_data_type_t type =
-//                     table_schema.column_data[col].data_type;
-
-//                 string value = current_row_values[col];
-
-//                 try
-//                 {
-//                     switch (type)
-//                     {
-//                         case INT:
-//                         {
-//                             int v = stoi(value);
-//                             cell.data = make_shared<int>(v);
-//                             break;
-//                         }
-
-//                         case UNSIGNED_INT:
-//                         {
-//                             unsigned int v = stoul(value);
-//                             cell.data = make_shared<unsigned int>(v);
-//                             break;
-//                         }
-
-//                         case STRING:
-//                         {
-//                             cell.data = make_shared<string>(value);
-//                             break;
-//                         }
-
-//                         case BOOLEAN:
-//                         {
-//                             bool v = (value == "true" || value == "1");
-//                             cell.data = make_shared<bool>(v);
-//                             break;
-//                         }
-
-//                         case FLOATING_POINT:
-//                         {
-//                             float v = stof(value);
-//                             cell.data = make_shared<float>(v);
-//                             break;
-//                         }
-
-//                         case DOUBLE_FLOATING_POINT:
-//                         {
-//                             double v = stod(value);
-//                             cell.data = make_shared<double>(v);
-//                             break;
-//                         }
-
-//                         case UNSIGNED_CHAR:
-//                         {
-//                             unsigned char v =
-//                                 static_cast<unsigned char>(stoi(value));
-//                             cell.data = make_shared<unsigned char>(v);
-//                             break;
-//                         }
-
-//                         case NULL_TYPE:
-//                         {
-//                             cell.data = nullptr;
-//                             break;
-//                         }
-
-//                         default:
-//                             logger("Unknown data type\n", LOG_ERROR);
-//                             return;
-//                     }
-//                 }
-//                 catch (...)
-//                 {
-//                     logger("Type conversion error\n", LOG_ERROR);
-//                     return;
-//                 }
-
-//                 row_data.push_back(cell);
-//             }
-
-//             all_rows.push_back(row_data);
-//         }
-//         else if (tokens[i] == "," || tokens[i] == ";")
-//         {
-//             continue;
-//         }
-//         else
-//         {
-//             current_row_values.push_back(tokens[i]);
-//         }
-//     }
-
-//     // Write all rows
-//     for (auto &row : all_rows)
-//     {
-//         int result = append_record_to_table(table_schema, row);
-
-//         if (result == 0)
-//             logger("Successfully Written Data\n", LOG_SUCCESS);
-//         else
-//             logger("Error in writing data!\n", LOG_ERROR);
-//     }
-// }
-// #include "interpreter/interpreter.h"
-// #include "engine/TableRead.h"
-// #include "engine/TableWrite.h"
-
-// void append_row(vector<string> tokens) {
-//     string table_Name = tokens[2];
-//     table_Name += "__schema_data.bin";
-//     schema_t table_Schema_for_Append = get_schema_from_schema(table_Name);
-//     vector<row_data_t> data;
-//     int num_Cols = table_Schema_for_Append.num_cols;
-//     for (int i = 0; i<num_Cols; i++) {
-//         col_data_type_t dt = table_Schema_for_Append.column_data[i].data_type;
-//         switch (dt) {
-//             case INT:
-//                 data.push_back(
-//                     (row_data_t){(unsigned char)i, make_shared<int>(tokens[i+3])}
-//                 );
-//             case UNSIGNED_INT:
-//                 data.push_back(
-//                     (row_data_t){(unsigned char)i, make_shared<unsigned int>(tokens[i+3])}
-//                 );
-//             case STRING:
-//                 data.push_back(
-//                     (row_data_t){(unsigned char)i, make_shared<string>(tokens[i+3])}
-//                 );
-//             case BOOLEAN:
-//                 data.push_back(
-//                     (row_data_t){(unsigned char)i, make_shared<bool>(tokens[i+3])}
-//                 );
-//             case FLOATING_POINT:
-//                 data.push_back(
-//                     (row_data_t){(unsigned char)i, make_shared<float>(tokens[i+3])}
-//                 );
-//             case DOUBLE_FLOATING_POINT:
-//                 data.push_back(
-//                     (row_data_t){(unsigned char)i, make_shared<double>(tokens[i+3])}
-//                 );
-//             case UNSIGNED_CHAR:
-//                 data.push_back(
-//                     (row_data_t){(unsigned char)i, make_shared<unsigned char>(tokens[i+3])}
-//                 );
-//             case NULL_TYPE:
-
-//         }
-
-//     }
-
-//     int result = append_record_to_table(table_Schema_for_Append, data);
-
-//     if (result == 0) {
 //         printf("row insertion successful! \n");
-//     } else {
+//     }
+//     else
+//     {
 //         printf("error! \n");
 //     }
 // }
-
-// INSERT INTO table_name
-// VALUES (value1, value2, value3, ...);
 void insert_into_table(vector<string> tokens)
 {
-    string table_name = tokens[2]+"__schema_data.bin";
-    schema_t new_schema = get_schema_from_schema(table_name);
-    //   string table=tokens[2];
-    // schema_t new_schema = get_schema_from_schema(table+"__schema_data.bin");
-    // for (size_t i = 0; i < new_schema.num_cols; i++)
-    // {
-    //     cout << new_schema.column_data[i].is_primary_key << " ";
-    //     cout << (int)new_schema.column_data[i].col_id << " ";
-    //     cout << (int)new_schema.column_data[i].data_type << " ";
-    //     cout << new_schema.column_data[i].col_name << " ";
-    //     cout << new_schema.column_data[i].is_string << " ";
-    //     cout << new_schema.column_data[i].max_str_len << " ";
-    //     cout << endl;
-    // }
+    string table_name = tokens[2];
+    schema_t new_schema = get_schema_from_schema(table_name + "__schema_data.bin");
+
     size_t num_cols = new_schema.num_cols;
-    vector<row_data_t> data_to_insert;
-    auto j = tokens.begin() + 5;
-    for (size_t i = 0; i < num_cols; i++)
+
+    auto j = tokens.begin() + 4;
+
+    while (j != tokens.end())
     {
+        if (*j == "(")
+        {
+            j++;
 
-        string value = *j;
-        cout << "Column " << i
-     << " datatype = "
-     << (int)new_schema.column_data[i].data_type<<" ID= "<<(int)new_schema.column_data[i].col_id
-     << endl;
-        switch (new_schema.column_data[i].data_type)
-        {
-        case INT:
-        {
-            data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<int>(stoi(value))});
-            j += 2;
-            break;
-        }
+            vector<row_data_t> data_to_insert;
 
-        case UNSIGNED_INT:
-        {
-            data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<unsigned int>(stoul(value))});
-            j += 2;
-            break;
-        }
+            for (size_t i = 0; i < num_cols; i++)
+            {
+                string value = *j;
 
-        case STRING:
-        {
-            data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<string>(value)});
-            j += 2;
-            break;
-        }
+                switch (new_schema.column_data[i].data_type)
+                {
+                case INT:
+                {
+                    data_to_insert.push_back(
+                        {static_cast<unsigned char>(i), make_shared<int>(stoi(value))});
+                    break;
+                }
 
-        case BOOLEAN:
-        {
-            bool v = (value == "true");
-            data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<bool>(v)});
-            j += 2;
-            break;
-        }
+                case UNSIGNED_INT:
+                {
+                    data_to_insert.push_back(
+                        {static_cast<unsigned char>(i), make_shared<unsigned int>(stoul(value))});
+                    break;
+                }
 
-        case FLOATING_POINT:
-        {
-            data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<float>(stof(value))});
-            j += 2;
-            break;
-        }
+                case STRING:
+                {
+                    data_to_insert.push_back(
+                        {static_cast<unsigned char>(i), make_shared<string>(value)});
+                    break;
+                }
 
-        case DOUBLE_FLOATING_POINT:
-        {
-            data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<double>(stod(value))});
-            j += 2;
-            break;
-        }
-        case UNSIGNED_CHAR:
-        {
-            unsigned char v = static_cast<unsigned char>(stoi(value));
-            data_to_insert.push_back(
-                (row_data_t){static_cast<unsigned char>(i), make_shared<unsigned char>(v)});
-            j += 2;
-            break;
-        }
+                case BOOLEAN:
+                {
+                    bool v = (value == "true");
+                    data_to_insert.push_back(
+                        {static_cast<unsigned char>(i), make_shared<bool>(v)});
+                    break;
+                }
 
-        case NULL_TYPE:
-        {
-            data_to_insert.push_back((row_data_t){static_cast<unsigned char>(i), make_shared<string>("NULL")});
-            j += 2;
-            break;
-        }
+                case FLOATING_POINT:
+                {
+                    data_to_insert.push_back(
+                        {static_cast<unsigned char>(i), make_shared<float>(stof(value))});
+                    break;
+                }
 
-        default:
-        {
-            logger("Unknown data type\n", LOG_ERROR);
-            return;
+                case DOUBLE_FLOATING_POINT:
+                {
+                    data_to_insert.push_back(
+                        {static_cast<unsigned char>(i), make_shared<double>(stod(value))});
+                    break;
+                }
+
+                case UNSIGNED_CHAR:
+                {
+                    unsigned char v = static_cast<unsigned char>(stoi(value));
+                    data_to_insert.push_back(
+                        {static_cast<unsigned char>(i), make_shared<unsigned char>(v)});
+                    break;
+                }
+
+                case NULL_TYPE:
+                {
+                    data_to_insert.push_back(
+                        {static_cast<unsigned char>(i), make_shared<string>("NULL")});
+                    break;
+                }
+
+                default:
+                {
+                    logger("Unknown data type\n", LOG_ERROR);
+                    return;
+                }
+                }
+
+                j++; // move to next token
+
+                if (*j == ",") // skip comma between values
+                    j++;
+            }
+
+            int result = append_record_to_table(new_schema, data_to_insert);
+
+            if (result != 0)
+            {
+                printf("error inserting row\n");
+                return;
+            }
+
+            // move until next row or end
+            while (j != tokens.end() && *j != "(")
+                j++;
         }
+        else
+        {
+            j++;
         }
     }
-    int result = append_record_to_table(new_schema, data_to_insert);
 
-    if (result == 0)
-    {
-        printf("row insertion successful! \n");
-    }
-    else
-    {
-        printf("error! \n");
-    }
+    printf("all rows inserted successfully!\n");
 }
--- src/interpreter/tokenizer.cpp
+++ src/interpreter/tokenizer.cpp
@@ -1,30 +1,68 @@
 #include "interpreter/interpreter.h"
 
-vector<string> tokenize(const string& s) {
+vector<string> tokenize(const string& s)
+{
     vector<string> tokens;
-    string x="";
-    int length=s.size();
-    for(int i=0;i<length;i++){
-        char c=s[i];
-        if(isspace(c)){
-            if(!x.empty()){
+    string x = "";
+    bool in_quotes = false;
+    char quote_char = 0;
+
+    for (size_t i = 0; i < s.size(); i++)
+    {
+        char c = s[i];
+
+        if ((c == '"' || c == '\'') && !in_quotes)
+        {
+            if (!x.empty())
+            {
                 tokens.push_back(x);
-                x="";
+                x = "";
             }
+
+            in_quotes = true;
+            quote_char = c;
             continue;
         }
-        else if((c==',')||(c=='(')||(c==')')||(c==';')||(c=='=')){
-                if(!x.empty()){
-                tokens.push_back(x);
-                x="";
-            }
-             tokens.push_back(string(1,c));
+
+        if (in_quotes && c == quote_char)
+        {
+            tokens.push_back(x);
+            x = "";
+            in_quotes = false;
+            continue;
         }
-        else{
-            x+=c;
+
+        if (in_quotes)
+        {
+            x += c;
+            continue;
+        }
+
+        if (isspace(c))
+        {
+            if (!x.empty())
+            {
+                tokens.push_back(x);
+                x = "";
+            }
+        }
+        else if (c == ',' || c == '(' || c == ')' || c == ';' || c == '=')
+        {
+            if (!x.empty())
+            {
+                tokens.push_back(x);
+                x = "";
+            }
+            tokens.push_back(string(1, c));
+        }
+        else
+        {
+            x += c;
         }
     }
-       if (!x.empty())
+
+    if (!x.empty())
         tokens.push_back(x);
+
     return tokens;
 }
