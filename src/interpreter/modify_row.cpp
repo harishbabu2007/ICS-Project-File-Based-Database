1ccfdfe22bb5f2c656d018f11d521dfd20daa0c9
rvrsiitj <b25me1063@iitj.ac.in>
2026-03-15 20:02:21 +0530

    Update modify_row.cpp

--- src/interpreter/modify_row.cpp
+++ src/interpreter/modify_row.cpp
@@ -6,9 +6,12 @@ void modify_row(vector<string> tokens) {
     string tbName = tokens[1];
     string schemaName = tbName + "__schema_data.bin";
     schema_t tbSchema = get_schema_from_schema(schemaName);
+    if (tbSchema.num_cols == 0) {
+        return;
+    }
     string givenCol = tokens[3];
     int numCols = tbSchema.num_cols;
-    int col;
+    int col = -1;
     cell_data_t after;
     for (int i = 0; i<numCols; i++) {
         if (tbSchema.column_data[i].col_name == givenCol) {
@@ -16,27 +19,44 @@ void modify_row(vector<string> tokens) {
             after.cell_data_type = tbSchema.column_data[i].data_type;
             switch (tbSchema.column_data[i].data_type)
             {
-            case INT:
-                after.cell_data = make_shared<int>(tokens[5]);
+            case INT: {
+                after.cell_data = make_shared<int>(std::stoi(tokens[5]));
                 break;
-            case UNSIGNED_INT:
-                after.cell_data = make_shared<unsigned int>(tokens[5]);
+            }
+            case UNSIGNED_INT: {
+                unsigned long temp = std::stoul(tokens[5]);
+                unsigned int x = static_cast<unsigned int>(temp);
+                after.cell_data = make_shared<unsigned int>(x);
                 break;
-            case STRING:
+            }
+            case STRING: {
                 after.cell_data = make_shared<string>(tokens[5]);
                 break;
-            case BOOLEAN:
-                after.cell_data = make_shared<bool>(tokens[5]);
+            }
+            case BOOLEAN: {
+                string s = "";
+                int l = tokens[5].size();
+                for (int y = 0; y<l; y++) {
+                    s += (char) tolower((unsigned char) tokens[5][y]);
+                }
+                bool b = (s == "true" || s == "1");
+                after.cell_data = make_shared<bool>(b);
                 break;
-            case FLOATING_POINT:
-                after.cell_data = make_shared<float>(tokens[5]);
+            }
+            case FLOATING_POINT: {
+                after.cell_data = make_shared<float>(stof(tokens[5]));
                 break;
-            case DOUBLE_FLOATING_POINT:
-                after.cell_data = make_shared<double>(tokens[5]);
+            }
+            case DOUBLE_FLOATING_POINT: {
+                after.cell_data = make_shared<double>(stod(tokens[5]));
                 break;
-            case UNSIGNED_CHAR:
-                after.cell_data = make_shared<unsigned char>(tokens[5]);
+            }
+            case UNSIGNED_CHAR: {
+                int temp = std::stoi(tokens[5]);
+                unsigned char c = static_cast<unsigned char>(temp);
+                after.cell_data = make_shared<unsigned char>(c);
                 break;
+            }
             default:
                 break;
             }
@@ -44,7 +64,7 @@ void modify_row(vector<string> tokens) {
         }
     }
     int numRows = tbSchema.num_rows;
-    int whereCol;
+    int whereCol = -1;
     string whereColumn = tokens[7];
     for (int i = 0; i<numCols; i++) {
         if (tbSchema.column_data[i].col_name == whereColumn) {
@@ -52,58 +72,56 @@ void modify_row(vector<string> tokens) {
             break;        
         }
     }
+    if (col == -1 || whereCol == -1) {
+        return;
+    }
     for (int j = 0; j<numRows; j++) {
-        switch (tbSchema.column_data[whereCol].data_type)
-        {
-        case INT:
-            if (std::stoi(tokens[9]) == *static_cast<int *>(get_table_cell_data(j, whereCol, tbSchema).cell_data.get())) {
+        cell_data_t cell = get_table_cell_data(j, whereCol, tbSchema);
+        if (tbSchema.column_data[whereCol].data_type == STRING) {
+            string cellVal = string(static_cast<char *>(cell.cell_data.get()));
+            if (tokens[9] == cellVal) {
                 cell_data_modify(tbSchema, after, j, col);
-                break;
             }
-            break;
-        case UNSIGNED_INT:
+        }
+        else if (tbSchema.column_data[whereCol].data_type == INT) {
+            if (std::stoi(tokens[9]) == *static_cast<int *>(cell.cell_data.get())) {
+                cell_data_modify(tbSchema, after, j, col);
+            }
+        }
+        else if (tbSchema.column_data[whereCol].data_type == UNSIGNED_INT) {
             unsigned long temp = std::stoul(tokens[9]);
             unsigned int x = static_cast<unsigned int>(temp);
-            if (x == *static_cast<unsigned int *>(get_table_cell_data(j, whereCol, tbSchema).cell_data.get())) {
+            if (x == *static_cast<unsigned int *>(cell.cell_data.get())) {
                 cell_data_modify(tbSchema, after, j, col);
-                break;
             }
-            break;
-        case STRING:
-            if (tokens[9] == *static_cast<string *>(get_table_cell_data(j, whereCol, tbSchema).cell_data.get())) {
+        }
+        else if (tbSchema.column_data[whereCol].data_type == BOOLEAN) {
+            string s = "";
+            int l = tokens[9].size();
+            for (int y = 0; y<l; y++) {
+                s += (char) tolower((unsigned char) tokens[9][y]);
+            }
+            bool b = (s == "true" || s == "1");
+            if (b == *static_cast<bool *>(cell.cell_data.get())) {
                 cell_data_modify(tbSchema, after, j, col);
-                break;
             }
-            break;
-        case BOOLEAN:
-            bool b = (tokens[9] == "true");
-            if (b == *static_cast<bool *>(get_table_cell_data(j, whereCol, tbSchema).cell_data.get())) {
+        }
+        else if (tbSchema.column_data[whereCol].data_type == FLOATING_POINT) {
+            if (std::stof(tokens[9]) == *static_cast<float *>(cell.cell_data.get())) {
                 cell_data_modify(tbSchema, after, j, col);
-                break;
-            }
-            break;
-        case FLOATING_POINT:
-            if (std::stof(tokens[9]) == *static_cast<float *>(get_table_cell_data(j, whereCol, tbSchema).cell_data.get())) {
+            }    
+        }
+        else if (tbSchema.column_data[whereCol].data_type == DOUBLE_FLOATING_POINT) {
+            if (std::stod(tokens[9]) == *static_cast<double *>(cell.cell_data.get())) {
                 cell_data_modify(tbSchema, after, j, col);
-                break;
-            }
-            break;
-        case DOUBLE_FLOATING_POINT:
-            if (std::stod(tokens[9]) == *static_cast<double *>(get_table_cell_data(j, whereCol, tbSchema).cell_data.get())) {
-                cell_data_modify(tbSchema, after, j, col);
-                break;
-            }
-            break;
-        case UNSIGNED_CHAR:
+            } 
+        }   
+        else if (tbSchema.column_data[whereCol].data_type == UNSIGNED_CHAR) {
             int temp = std::stoi(tokens[9]);
             unsigned char c = static_cast<unsigned char>(temp);
-            if (c == *static_cast<unsigned char *>(get_table_cell_data(j, whereCol, tbSchema).cell_data.get())) {
+            if (c == *static_cast<unsigned char *>(cell.cell_data.get())) {
                 cell_data_modify(tbSchema, after, j, col);
-                break;
-            }
-            break;
-        default:
-            break;
+            }   
         }
     }
 }
