94103308de4fd0da12069c2dbf045755b28a2655
rvrsiitj <b25me1063@iitj.ac.in>
2026-03-18 16:28:50 +0530

    Update displayTables.cpp

--- src/interpreter/displayTables.cpp
+++ src/interpreter/displayTables.cpp
@@ -1,27 +1,27 @@
 #include <interpreter/interpreter.h>
 
 void display_tables() {
-    namespace fs = std::filesystem;
-    std::string suffix = "__schema_data.bin";
+    namespace fs = filesystem;
+    string suffix = "__schema_data.bin";
     bool k = false;
     printf("displaying tables... \n");
 //__schema_data.bin is created for every table whether or not data exists in it
 //but _table_data is created only when data is inserted
 //so, we use schema files to get names of the tables
     try {
-        for (const auto &entry : fs::directory_iterator(".")) { //"." refers to the path of the current working direcotry
-            std::string filename = entry.path().filename().string();
-            if (filename.size() > suffix.size() && filename.compare(filename.size() - suffix.size(), suffix.size(), suffix) == 0) {
-                std::string tableName = filename.substr(0, filename.size() - suffix.size());
-                std::cout << "> " << tableName << std::endl;
+        for (const auto &e : fs::directory_iterator(".")) { //"." refers to the path of the current working direcotry
+            string f = e.path().filename().string();
+            if (f.size() > suffix.size() && f.compare(f.size() - suffix.size(), suffix.size(), suffix) == 0) {
+                string tableName = f.substr(0, f.size() - suffix.size());
+                cout << "> " << tableName << endl;
                 k = true;
             }
         }
-    } catch (const fs::filesystem_error &e) {
-        std::cerr << "Error accessing directory: " << e.what() << std::endl;
+    } catch (const fs::filesystem_error &error) {
+        cerr << "Error accessing directory: " << error.what() << endl;
     }
 
     if (!k) {
-        std::cout << "no tables..." << std::endl;
+        cout << "no tables..." << endl;
     }
 }
